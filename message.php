f90b2a945363a54238705445f87bc13aa39d904c
mhemanthkmr <m.hemanthkmr143@gmail.com>
2022-01-08 21:41:49 +0530

    Every thiing is working perfectly

---
+++ message.php
@@ -0,0 +1,11 @@
+<?php 
+// session_start();
+if(isset($_SESSION['message']))
+{ ?>
+        <h4><?=$_SESSION['message']?></h4> 
+    <?php
+        unset($_SESSION['message']);
+
+}
+
+?>
--- register.php
+++ register.php
@@ -1,4 +1,5 @@
 <?php 
+session_start();
 include('includes/header.php');
 ?>
 
@@ -6,12 +7,13 @@ include('includes/header.php');
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-5">
+                <?php include('message.php')?>
                 <div class="card">
                     <div class="card-header">
                         <h4>Register</h4>
                     </div>
                     <div class="card-body">
-                        <form action="code.php" method="post" class="">
+                        <form action="registercode.php" method="POST" class="">
                             <div class="form-group mb-3">
                                 <label for="name">Name</label>
                                 <input type="name" placeholder="Enter Name" class = "form-control" name="name">
@@ -23,6 +25,10 @@ include('includes/header.php');
                             <div class="form-group mb-3">
                                 <label for="password">Password</label>
                                 <input type="password" placeholder="Enter Password" class = "form-control" name="password">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="password">Confirm Password</label>
+                                <input type="password" placeholder="Enter Confirm Password" class = "form-control" name="cpassword">
                             </div>
                             <div class="form-group mb-3 text-center">
                                 <button class="btn btn-primary" name = "register_btn" type="submit">Register</button>
