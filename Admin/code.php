bb2889469ce0cec2a4b0f44f1d6b68d5dd379f18
mhemanthkmr <m.hemanthkmr143@gmail.com>
2022-01-12 22:02:00 +0530

    added update category

--- Admin/code.php
+++ Admin/code.php
@@ -1,6 +1,45 @@
 <?php 
 include('authentication.php');
 // die(print_r($_POST));
+
+if(isset($_POST['update-category']))
+{
+    // die(print_r($_POST));
+    $id = $_POST['id'];
+    // die($id);
+    $name = $_POST['name'];
+    $slug = $_POST['slug'];
+    $description = $_POST['description'];
+    $meta_title = $_POST['meta-title'];
+    $meta_description = $_POST['meta-description'];
+    $meta_keyword = $_POST['meta-keyword'];
+    $navbar_status = $_POST['navbar-status'] == true ? '1' : '0';
+    $status = $_POST['status'] == true ? '1' : '0';
+
+
+    // $query = "INSERT INTO categories(name,slug,description,meta-title,meta-description,meta-keyword,navbar-status,status) VALUES ('$name','$slug','$description','$meta_title','$meta_description','$meta_keyword','$navbar_status','$status')";
+    $query1 = "INSERT INTO `categories` (`name`, `slug`, `description`, `meta-title`, `meta-description`, `meta-keyword`, `navbar-status`, `status`) VALUES ('$name','$slug','$description','$meta_title','$meta_description','$meta_keyword','$navbar_status','$status')";
+    $query = "UPDATE `categories` SET `name` = '$name', `slug` = '$slug', `description` = '$description', `meta-title` = '$meta_title', `meta-description` = '$meta_description', `meta-keyword` = '$meta_keyword', `navbar-status` = '$navbar_status', `status` = '$status' WHERE (`id` = '$id');";
+    // die($query);
+    // die($query);
+    $query_run = mysqli_query($con,$query);
+    if($query_run)
+    {
+        $_SESSION['flag'] = 1;
+        $_SESSION['message'] = "Catogory Updated Successfully";
+        header("Location: category-view.php");
+        exit(0);
+    }
+    else 
+    {
+        $_SESSION['flag'] = 2;
+        $_SESSION['message'] = "Something Wrong";
+        header("Location: category-add.php");
+        exit(0);
+    }
+}
+
+
 if(isset($_POST['add-category']))
 {
     // die(print_r($_POST));
@@ -15,7 +54,7 @@ if(isset($_POST['add-category']))
 
 
     // $query = "INSERT INTO categories(name,slug,description,meta-title,meta-description,meta-keyword,navbar-status,status) VALUES ('$name','$slug','$description','$meta_title','$meta_description','$meta_keyword','$navbar_status','$status')";
-    $query1 = "INSERT INTO `categories` (`name`, `slug`, `description`, `meta-title`, `meta-description`, `meta-keyword`, `navbar-status`, `status`) VALUES ('$name','$slug','$description','$meta_title','$meta_description','$meta_keyword','$navbar_status','$status')";
+    $query1 = "INSERT INTO 'categories` (`name`, `slug`, `description`, `meta-title`, `meta-description`, `meta-keyword`, `navbar-status`, `status`) VALUES ('$name','$slug','$description','$meta_title','$meta_description','$meta_keyword','$navbar_status','$status')";
     $query_run = mysqli_query($con,$query1);
     if($query_run)
     {
@@ -85,7 +124,7 @@ if(isset($_POST['update_user']))
 }
 else 
 {
-    die("Hee");
+    die();
 
 }
 
