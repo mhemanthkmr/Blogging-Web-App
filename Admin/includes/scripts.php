786031db5d88a64e3109ddfd30acf220d4085823
mhemanthkmr <m.hemanthkmr143@gmail.com>
2022-01-18 22:42:36 +0530

    added summernote scripts

--- Admin/includes/scripts.php
+++ Admin/includes/scripts.php
@@ -1,9 +1,23 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="js/scripts.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
         <script src="assets/demo/chart-area-demo.js"></script>
         <script src="assets/demo/chart-bar-demo.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
         <script src="js/datatables-simple-demo.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js""></script>
+            <!-- Summernote JS - CDN Link -->
+        <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
+        <script>
+            $(document).ready(function() {
+                $("#summernote").summernote({
+                    placeholder: 'Type Your Description',
+                    tabsize: 2,
+                    height: 300
+                });
+                $('.dropdown-toggle').dropdown();
+            });
+        </script>
+        <!-- //Summernote JS - CDN Link -->
     </body>
 </html>
