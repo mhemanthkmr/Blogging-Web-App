a16c12a2d4ff965cbe2d7d07177f45cdc2bd0687
mhemanthkmr <m.hemanthkmr143@gmail.com>
2022-01-18 22:42:19 +0530

    added summer note css cdn

--- Admin/includes/header.php
+++ Admin/includes/header.php
@@ -9,6 +9,10 @@
         <title>Admin Dashboard</title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
         <link href="css/styles.css" rel="stylesheet" />
+        <!-- Summernote CSS - CDN Link -->
+        <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
+        <!-- //Summernote CSS - CDN Link -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
     </head>
 
